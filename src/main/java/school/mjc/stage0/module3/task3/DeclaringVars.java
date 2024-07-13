d9aed313e1898b0b285f38b87aa1bf54ab0d8887
lownn <kurilyakoleksii12@gmail.com>
2024-07-13 13:44:37 +0200

    aaaaaa

--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -3,7 +3,10 @@ package school.mjc.stage0.module3.task3;
 public class DeclaringVars {
     public static void main(String[] args) {
         //write code here
-    	int a = 10, b = a, c = b;
-    	System.out.println(a);
+    	int a, b, c;
+    	a = 10;
+    	b = a;
+    	c = b;
+    	System.out.println(c);
     }
 }
